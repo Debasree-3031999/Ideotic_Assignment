94ebbfc6410e5482ef3772db741638b0cbb4784e
Debasree Ghosh <97526617+Debasree-3031999@users.noreply.github.com>
2023-01-12 00:27:42 +0530

    fixing server

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,12 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
-
+const PORT=process.env.PORT || 3000
 server.use(middlewares)
-// Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
-server.use(router)
-server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
 
-// Export the Server API
+
+server.use(router)
+server.listen(PORT, () => {
+    console.log(`JSON Server is running at ${PORT}`)
+})
 module.exports = server
